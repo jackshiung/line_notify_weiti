fa3646fda8eb900cdbf34cbbb66fce01e91b6ff4
TeddieHsiung <jackshiung@gmail.com>
2022-09-01 02:14:53 +0800

    20220901-v1

---
+++ Line_Notify_Weiti/Controllers/JJNotifyController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using isRock.LineNotify;
+using Newtonsoft.Json.Linq;
+
+namespace Line_Notify_Example.Controllers
+{
+    public class JJNotifyController: ApiController
+    {
+        [HttpPost]
+        public HttpResponseMessage Post(NotifyInput notifyInput)
+        {
+            HttpResponseMessage res = new HttpResponseMessage();
+            var ret = Utility.SendNotify(notifyInput.Token, notifyInput.Content);
+            res.Content = new ObjectContent<JObject>(JObject.FromObject(new { Msg = ret.message }), new JsonMediaTypeFormatter());
+            return res;
+        }
+
+        public class NotifyInput
+        {
+            public string Token { get; set; }
+            public string Content { get; set; }
+        }
+    }
+}
